c7779519968e4310f2ed32e4d6fb8e97da365237
Jason Kozel <jason.kozel@crowdbotics.com>
2023-06-16 11:18:59 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
-import { View, Image, StyleSheet, ImageBackground } from "react-native";
+import { View, StyleSheet, ImageBackground } from "react-native";
 
 const Splash = ({
   navigation
@@ -11,10 +11,10 @@ const Splash = ({
     }, 2000);
   }, []);
   return <View style={styles.container}>
-      <ImageBackground style={styles.BAuRCnCj} source={require("./assets/Wheel_Loader_WA500.png")} resizeMode="cover"></ImageBackground>
-      <ImageBackground style={styles.eziaingY} source={require("./assets/komatsu-logo.png")} resizeMode="cover"></ImageBackground>
-      <ImageBackground style={styles.BAuRCnCjw} source={require("./assets/bg.png")} resizeMode="cover"></ImageBackground>
-      <Image source={require("./Wheel_Loader_WA500.png")} style={styles.logo} />
+      <ImageBackground style={styles.BAuRCnCj} source={require("./Wheel_Loader_WA500.png")} resizeMode="cover"></ImageBackground>
+      <ImageBackground style={styles.eziaingY} source={require("./komatsu-logo.png")} resizeMode="cover"></ImageBackground>
+      <ImageBackground style={styles.BAuRCnCjw} source={require("./bg.png")} resizeMode="cover"></ImageBackground>
+
     </View>;
 };
 
@@ -27,6 +27,34 @@ const styles = StyleSheet.create({
   logo: {
     width: 300,
     height: 230
+  },
+  BAuRCnCj: {
+    width: 359,
+    height: 286,
+    position: "absolute",
+    bottom: 0,
+    alignSelf: "center",
+    top: 175.5
+  },
+  BAuRCnCjw: {
+    width: 999,
+    height: 361,
+    position: "absolute",
+    bottom: 0,
+    alignSelf: "center",
+    top: 119,
+    zIndex: -1,
+    transform: [{
+      rotate: "90deg"
+    }],
+    left: -320
+  },
+  eziaingY: {
+    width: 192,
+    height: 37,
+    position: "absolute",
+    top: 50,
+    alignSelf: "center"
   }
 });
 export default Splash;
