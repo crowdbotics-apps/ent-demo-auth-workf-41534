12d01b9109363872f382a962d863ace30b936934
Jason Kozel <jason.kozel@crowdbotics.com>
2023-06-16 10:29:01 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "ent-demo-auth-workf-41534",
-  url: "https://ent-demo-auth-workf-41534.botics.co",
-  api: "https://ent-demo-auth-workf-41534.botics.co/api/v1"
-}
+  "name": "ent-demo-auth-workf-41534",
+  "url": "https://ent-demo-auth-workf-41534.botics.co",
+  "api": "https://ent-demo-auth-workf-41534.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
